fd53c8f22ba413b30cf6b7652b810edc0d3c4fa6
NamaMedical <nama@medical.com>
2026-07-28 22:23:39 +0300

    P3-LE v3.263.0: 3 PCC modules (pcc_neuro_ext164, pcc_pediatric_neuro_ext153, pcc_pediatric_surg_ext153) - 180 tests pass

---
+++ pcc/pcc_pediatric_neuro_ext153/pcc_pediatric_neuro_ext153_up.sql
@@ -0,0 +1,2 @@
+-- pcc_pediatric_neuro_ext153 up
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[pcc_pediatric_neuro_ext153]') AND type = 'U') BEGIN CREATE TABLE [dbo].[pcc_pediatric_neuro_ext153] ([id] INT IDENTITY(1,1) PRIMARY KEY,[tenant_id] NVARCHAR(64) NOT NULL,[version] NVARCHAR(16) NOT NULL,[module] NVARCHAR(64) NOT NULL,[function] NVARCHAR(128) NOT NULL,[input] NVARCHAR(MAX) NULL,[score] DECIMAL(6,2) NULL,[ts] DATETIME2 DEFAULT SYSUTCDATETIME()); END
