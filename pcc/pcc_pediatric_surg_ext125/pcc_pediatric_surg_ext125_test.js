1a8d977143de19c056d801399f79b830a74a578f
NamaMedical <nama@medical.com>
2026-07-28 22:06:28 +0300

    P3-KC v3.235.0: 3 PCC modules (pcc_neuro_ext136, pcc_pediatric_neuro_ext125, pcc_pediatric_surg_ext125) - 180 tests pass

---
+++ pcc/pcc_pediatric_surg_ext125/pcc_pediatric_surg_ext125_test.js
@@ -0,0 +1,1 @@
+const {functions:F}=require('./pcc_pediatric_surg_ext125_engine');let pass=0;for(const fn of Object.keys(F)){const r=F[fn]({});if(typeof r.score==='number'&&r.score>=0&&r.score<=1.5)pass++;}console.log('pcc_pediatric_surg_ext125 unit:',pass);if(pass!==10)process.exit(1);
